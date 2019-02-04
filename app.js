4f88e1e3e09d6fab004107b9ab402f9b08c84619
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-04 11:26:36 +0700

    Improve code quality

--- app.js
+++ app.js
@@ -1,56 +1,58 @@
-let tasks = ['Learning JavaScript', 'coba'];
+let tasks = ['Learning JavaScript', 'coba']
 
-const taskListDOM = document.getElementById('list-todolist');
+const taskListDOM = document.getElementById('list-todolist')
 
 function display() {
-  let number = 0;
-  taskListDOM.innerHTML = '';
+  let number = 0
+
+  taskListDOM.innerHTML = ''
   tasks.forEach(task => {
-    const listDOM = document.createElement('li');
-    const deleteButton = document.createElement('BUTTON');
-    const textDOM = document.createTextNode(task);
+    const listDOM = document.createElement('li')
+    const deleteButton = document.createElement('BUTTON')
+    const textDOM = document.createTextNode(task)
 
-    deleteButton.setAttribute('id', 'button-delete');
-    deleteButton.setAttribute('type', 'submit');
-    deleteButton.setAttribute('onclick', `deleteTask( ${number++} )`);
+    deleteButton.setAttribute('id', 'button-delete')
+    deleteButton.setAttribute('type', 'submit')
+    deleteButton.setAttribute('onclick', `deleteTask( ${number++} )`)
 
-    const button = document.createTextNode('delete');
-    deleteButton.appendChild(button);
+    const button = document.createTextNode('delete')
+    deleteButton.appendChild(button)
 
-    listDOM.appendChild(textDOM); // into LI
-    listDOM.appendChild(deleteButton);
+    listDOM.appendChild(textDOM) // into LI
+    listDOM.appendChild(deleteButton)
 
-    taskListDOM.appendChild(listDOM); // into UL
-  });
-  number = 0;
+    taskListDOM.appendChild(listDOM) // into UL
+  })
 }
 
-function Submit() {
-  event.preventDefault();
-  const todo = document.getElementById('input-text').value; // string value
-  console.log(todo);
+function submitTask() {
+  event.preventDefault()
+
+  const todo = document.getElementById('input-text').value // string value
+  console.log(todo)
+
   if (todo !== '') {
-    tasks.push(todo);
-    console.log(tasks);
-    display();
-    document.getElementById('input-text').value = '';
+    tasks.push(todo)
+    console.log(tasks)
+    display()
+    document.getElementById('input-text').value = ''
   }
 }
 
 function deleteTask(number) {
-  event.preventDefault();
+  event.preventDefault()
 
   if (confirm('Are you sure, delete this list?')) {
     const deleted = tasks.filter((todo, index) => {
-      return index !== number;
-    });
-    tasks = deleted;
-    console.log(tasks);
-    display();
-    console.log(number);
-  } else {
-    txt = 'You pressed Cancel!';
+      return index !== number
+    })
+
+    tasks = deleted
+    console.log(tasks)
+
+    display()
+    console.log(number)
   }
 }
 
-display();
+display()
